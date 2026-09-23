29bd6c09a19f137401d25f2e0069b55fa5eefa09
Atxvy <automacrohub@gmail.com>
2026-09-23 04:59:03 -0700

    Create PremConfig.lua

---
+++ PremConfig.lua
@@ -0,0 +1,318 @@
+return {
+
+RevampAutoTrials = {
+    ["Speedy Enemies"] = {
+        Level = 175,
+        Maps = {"Wrecked Battlefield"},
+        Towers = {
+            ["Tower 1"] = {"Tesla", "Gatling Gun", "Medic", "Mercenary Base", "Trapper"},
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Speedy.lua", -- done
+        }
+    },
+    ["Glass"] = {
+        Level = 175,
+        Maps = {"Stained Temple"},
+        Towers = {
+            ["Tower 1"] = {"Medic", "Gatling Gun", "Militant", "Mercenary Base", "Trapper"},
+			["Tower 2"] = {"Hacker", "Gatling Gun", "Medic", "Mercenary Base", "Trapper"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Glass.lua", --done
+			["Tower 2"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Premium/Glass.lua" -- done
+        }
+    },
+    ["Quarantine"] = {
+        Level = 175,
+        Maps = {"Dusty Bridges"},
+        Towers = {
+            ["Tower 1"] = {"Trapper", "Gatling Gun", "Militant", "Mercenary Base", "Medic"},
+			["Tower 2"] = {"Hacker", "Gatling Gun", "Militant", "Mercenary Base", "Medic"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Quarantine.lua", -- done
+			["Tower 2"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Premium/Quarantine.lua" -- done
+        }
+    },
+    ["Fog"] = {
+        Level = 175,
+        Maps = {"Winter Abyss"},
+        Towers = {
+            ["Tower 1"] = {"Trapper", "Militant", "Gatling Gun", "Mercenary Base", "Medic"},
+			["Tower 2"] = {"Trapper", "Hacker", "Gatling Gun", "Mercenary Base", "Medic"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Fog.lua", --done
+			["Tower 2"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Premium/Fog.lua" -- done
+        }
+    },
+    ["Limitation"] = {
+        Level = 175,
+        Maps = {"Coral Deep"},
+        Towers = {
+            ["Tower 1"] = {"Trapper", "Medic", "Gatling Gun", "Mercenary Base", "DJ Booth"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Limitation.lua"
+        }
+    },
+    ["Flying Enemies"] = {
+        Level = 175,
+        Maps = {"Sacred Mountains"},
+        Towers = {
+            ["Tower 1"] = {"Militant", "Gatling Gun", "Medic", "Mercenary Base", "DJ Booth"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Flying.lua"
+        }
+    },
+    ["Jailed"] = {
+        Level = 175,
+        Maps = {"Night Station"},
+        Towers = {
+            ["Tower 1"] = {"Assassin", "Scout", "Paintballer", "DJ Booth", "Gatling Gun"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Jailed.lua" -- done
+        }
+    },
+    ["Exploding Enemies"] = {
+        Level = 175,
+        Maps = {"Wrecked Battlefield II"},
+        Towers = {
+            ["Tower 1"] = {"Militant", "Gatling Gun", "Medic", "Mercenary Base", "Trapper"},
+			["Tower 2"] = {"Hacker", "Gatling Gun", "Medic", "Mercenary Base", "Trapper"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Exploading.lua", -- done
+			["Tower 2"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Exploading.lua" --done
+        }
+    },
+    ["Inflation"] = {
+        Level = 175,
+        Maps = {"Cyber City"},
+        Towers = {
+            ["Tower 1"] = {"Ace Pilot", "Trapper", "Gatling Gun", "Tesla", "Medic"},
+			["Tower 2"] = {"Ace Pilot", "Trapper", "Gatling Gun", "Hacker", "Medic"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Inflation.lua", -- done
+			["Tower 2"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Premium/Inflation.lua", -- done
+        }
+    },
+    ["Committed"] = {
+        Level = 175,
+        Maps = {"Retro Zone"},
+        Towers = {
+            ["Tower 1"] = {"Hacker", "Gatling Gun", "Medic", "Scout", "Demoman"}
+        },
+        Golden = {"Scout", "Demoman"},
+        SkillTree = {
+            ["Bigger Budget"] = 25,
+            ["Fortify"] = 40,
+            ["Stonks"] = 20,
+            ["Over-Heal"] = 25,
+            ["Bandages"] = 25,
+            ["Accelerator"] = 25,
+            ["Enhanced Optics"] = 20,
+            ["Scavenger"] = 20,
+            ["Improved Gunpowder"] = 25,
+            ["Fight Dirty"] = 25,
+            ["Precision"] = 15,
+            ["Re-enforcements"] = 10,
+            ["Extreme Conditioning"] = 25,
+        },
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Committed.lua" -- done
+        }
+    },
+    ["Hidden Enemies"] = {
+        Level = 175,
+        Maps = {"Forgetten Docks"},
+        Towers = {
+            ["Tower 1"] = {"Gatling Gun", "Medic", "Mercenary Base", "Militant", "DJ Booth"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Hidden.lua"
+        }
+    },
+    ["Broke"] = {
+        Level = 175,
+        Maps = {"Medieval Times"},
+        Towers = {
+            ["Tower 1"] = {"Gatling Gun", "Trapper", "Militant", "Mercenary Base", "Medic"},
+            ["Tower 2"] = {"Gatling Gun", "Hacker", "Militant", "Mercenary Base", "Medic"}
+        },
+        Golden = {},
+        SkillTree = {},
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Broke.lua", -- done
+            ["Tower 2"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Premium/Broke.lua" -- done
+        }
+    },
+    ["Healthy Enemies"] = {
+        Level = 175,
+        Maps = {"Four Seasons"},
+        Towers = {
+            ["Tower 1"] = {"Ace Pilot", "Mercenary Base", "DJ Booth", "Gatling Gun", "Medic"}
+        },
+        Golden = {},
+        SkillTree = {
+            ["Bigger Budget"] = 10,
+            ["Fortify"] = 10,
+            ["Stonks"] = 10,
+            ["Over-Heal"] = 10,
+            ["Bandages"] = 10,
+            ["Accelerator"] = 10,
+            ["Enhanced Optics"] = 10,
+            ["Resourcefulness"] = 10,
+        },
+        scripts = {
+            ["Tower 1"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Free/Healthy.lua" -- done
+        }
+    },
+},
+    
+RevampedFallbackConfigs = {
+    ["Molten"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Lay By"},
+        Modifiers = {
+            HiddenEnemies = true, 
+            Glass = false, 
+            Fog = true, 
+            Limitation = true, 
+            Committed = true, 
+            Quarantine = true, 
+            ExplodingEnemies = true
+        },
+        Scripts = {
+            ["Lay By"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Fallbacks/MoltenLayby.lua", -- done
+        },
+    },
+    ["Fallen"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Lay By"},
+        Modifiers = {
+            HiddenEnemies = true, 
+            Broke = true, 
+            Fog = true, 
+            Limitation = true, 
+            Committed = true, 
+            Quarantine = true, 
+            ExplodingEnemies = true
+        },
+        Scripts = {
+            ["Lay By"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Trials/Fallbacks/FallenLayby.lua", --done
+        },
+    },
+    ["Hardcore"] = {
+        Level = 50,
+        Mode = "hardcore",
+        Towers = {"Farm", "Boomerang", "Crook Boss"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Wretched Front"},
+        Modifiers = {},
+        Scripts = {
+            ["Wretched Front"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Currency/Gems/Lose/WretchedFront.lua", -- done
+        },
+    },
+},
+
+CrateConfigs = {
+    Gems = {
+        Lose = {
+            Level = 50,
+            Mode = "hardcore",
+            Towers = {"Farm", "Boomerang", "Crook Boss"},
+            Golden = {},
+            SkillTree = {},
+            Maps = {"Wretched Front"},
+            Scripts = {
+                ["Wretched Front"] = "https://raw.githubusercontent.com/Atxvy/Main/refs/heads/main/Currency/Gems/Lose/WretchedFront.lua", -- done
+            },
+        },
+    },
+},
+
+TowerList = {
+     ["Coins"] = {
+        { Name = "Scout", Cost = 0 },
+        { Name = "Sniper", Cost = 50 },
+        { Name = "Paintballer", Cost = 100 },
+        { Name = "Demoman", Cost = 200 },
+        { Name = "Boomerang", Cost = 300 },
+        { Name = "Slime Trooper", Cost = 300 },
+        { Name = "Soldier", Cost = 350 },
+        { Name = "Freezer", Cost = 650 },
+        { Name = "Militant", Cost = 800 },
+        { Name = "Assassin", Cost = 800 },
+        { Name = "Shotgunner", Cost = 850 },
+        { Name = "Hunter", Cost = 1000 },
+        { Name = "Pyromancer", Cost = 1250 },
+        { Name = "Ace Pilot", Cost = 1500 },
+        { Name = "Farm", Cost = 2000 },
+        { Name = "Medic", Cost = 2000 },
+        { Name = "Rocketeer", Cost = 2500 },
+        { Name = "Electroshocker", Cost = 2500 },
+        { Name = "Trapper", Cost = 3000 },
+        { Name = "Pulse Trooper", Cost = 3250 },
+      --  { Name = "Cowboy", Cost = 4000 }, -- Paid via Robux / Special Unlock
+        { Name = "Commander", Cost = 4000 },
+      --  { Name = "Warden", Cost = 4000 }, -- Paid via Robux / Special Unlock
+        { Name = "Military Base", Cost = 4000 },
+        { Name = "DJ Booth", Cost = 5000 },
+        { Name = "Tesla", Cost = 6000 },
+      --  { Name = "Saboteur", Cost = 7500 }, -- Paid via Robux
+        { Name = "Minigunner", Cost = 8000 },
+        { Name = "Ranger", Cost = 12000 },
+        { Name = "Pursuit", Cost = 15000 },
+        { Name = "Gatling Gun", Cost = 35000 },
+    },
+    ["Gems"] = {
+        { Name = "Accelerator", Cost = 2500 },
+        { Name = "Brawler", Cost = 1250 },
+        { Name = "Necromancer", Cost = 2250 },
+        { Name = "Engineer", Cost = 4500 },
+		{ Name = "Hacker", Cost = 5500 },
+    },
+    ["Golden"] = {
+        { Name = "Golden Scout", Cost = 50000 },
+		{ Name = "Golden Demoman", Cost = 50000 },
+        { Name = "Golden Soldier", Cost = 50000 },
+        { Name = "Golden Pyromancer", Cost = 50000 },
+        { Name = "Golden Crook Boss", Cost = 50000 },
+        { Name = "Golden Minigunner", Cost = 50000 },
+        { Name = "Golden Cowboy", Cost = 50000 },
+    },
+},
+
+}
